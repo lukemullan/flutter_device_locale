3d9ad31c2dcdd05bd46cbbdfb2a8c4b38ffc3876
David Lawson <hello@davidlawson.com.au>
2021-06-15 14:37:22 +1000

    migrating to null safety

--- lib/src/method_channel.dart
+++ lib/src/method_channel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/services.dart';
 
 import 'platform.dart';
+import 'dart:async';
 
 const MethodChannel _channel = MethodChannel('flutter_device_locale');
 
@@ -8,7 +9,7 @@ class FlutterDeviceLocaleMethodChannelPlugin
     extends FlutterDeviceLocalePlatform {
   @override
   Future<List<String>> deviceLocales() async {
-    final List<dynamic> result = await _channel.invokeMethod('deviceLocales');
+    final result = (await _channel.invokeMethod('deviceLocales')) as List;
     return result.cast<String>();
   }
 }
